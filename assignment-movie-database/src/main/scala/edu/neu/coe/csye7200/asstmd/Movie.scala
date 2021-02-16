8a825d97a766b8f3c069d1c897e3238a24272d29
lycoding27 <55034861+lycoding27@users.noreply.github.com>
2021-02-16 02:25:09 -0500

    Add files via upload

--- assignment-movie-database/src/main/scala/edu/neu/coe/csye7200/asstmd/Movie.scala
+++ assignment-movie-database/src/main/scala/edu/neu/coe/csye7200/asstmd/Movie.scala
@@ -98,7 +98,7 @@ object Movie extends App {
       * @param w a line of input.
       * @return a Try[Movie]
       */
-    def parse(w: String): Try[Movie] = ??? // TO BE IMPLEMENTED
+    def parse(w: String): Try[Movie] = Try(apply(w.split(","))) // TO BE IMPLEMENTED
   }
 
   val ingester = new Ingest[Movie]()
@@ -121,7 +121,7 @@ object Movie extends App {
     // 6 points
     val result: Seq[String] =
     // TO BE IMPLEMENTED
-    ???
+    for(index <- indices) yield list(index)
     result.toList
   }
 
@@ -201,7 +201,11 @@ object Rating {
     */
   // Hint: This should similar to apply method in Object Name. The parameter of apply in case match should be same as case class Rating
   // 13 points
-  def apply(s: String): Rating = ??? // TO BE IMPLEMENTED
+  def apply(s: String): Rating = s match {
+    case rRating(code, _, null) => apply(code, None)
+    case rRating(code, _, age) => apply(code, Some(age.toInt))
+    case _ =>  throw new Exception(s"logic error in Principal: $s")
+  }  // TO BE IMPLEMENTED
 
 }
 
